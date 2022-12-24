fd5c4b685f48f9513866e8c4831d70b0af4c5e36
Eosst <medmimouni98@gmail.com>
2022-12-24 20:10:56 +0100

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolProject1.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public byte[] Picture { get; set; }
+
+    }
+}
---
+++ data/ApplicationDBcontext.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SchoolProject1.Models;
+
+namespace SchoolProject1.data
+{
+    public class ApplicationDBcontext:IdentityDbContext
+    {
+        public ApplicationDBcontext(DbContextOptions<ApplicationDBcontext> options) : base(options) { }
+        public DbSet<SchoolProject1.Models.Product> Product { get; set; }
+    }
+}
